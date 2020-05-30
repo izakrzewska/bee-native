1fb81e9aabc02f6b395dd4dee9eda20345a386b9
Iga Zakrzewska <igazakrzewska@MacBook-Air-Iga.local>
2020-05-30 14:32:36 +0200

    add apiaries card

--- screens/ApiariesScreen.tsx
+++ screens/ApiariesScreen.tsx
@@ -1,12 +1,32 @@
 import React from 'react';
-import { Container, Text } from 'native-base';
+import { useSelector } from 'react-redux';
+import { Image } from 'react-native';
+import { Container, Content, Card, CardItem, Text } from 'native-base';
+import { RootState } from '../store/types';
 
 const ApiariesScreen = () => {
+  const apiaries = useSelector((state: RootState) => state.apiaries.apiaries);
+
   return (
-    <Container
-      style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
-    >
-      <Text>Apiaries Screen</Text>
+    <Container>
+      <Content style={{ margin: 20 }}>
+        {apiaries.map((apiary) => (
+          <Card key={apiary.id}>
+            <CardItem header>
+              <Text>{apiary.name}</Text>
+            </CardItem>
+            <CardItem cardBody>
+              <Image
+                source={require('../assets/apiary.jpg')}
+                style={{ height: 200 }}
+              />
+            </CardItem>
+            <CardItem footer>
+              <Text>{apiary.beehives.length} uli</Text>
+            </CardItem>
+          </Card>
+        ))}
+      </Content>
     </Container>
   );
 };
