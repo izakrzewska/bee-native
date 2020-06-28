fde1bf1b8f417d1896504eb0219c40d66cc2a8fd
Iga Zakrzewska <igazakrzewska@MacBook-Air-Iga.local>
2020-06-28 20:59:08 +0200

    add redux-thunk

--- store/actions/apiaries.ts
+++ store/actions/apiaries.ts
@@ -1,8 +1,11 @@
 import { Apiary, ADD_APIARY, ApiaryActionTypes } from '../types';
+import { Dispatch } from 'redux';
 
-export const addApiary = (newApiary: Apiary): ApiaryActionTypes => {
-  return {
+export const addApiary = (newApiary: Apiary) => (
+  dispatch: Dispatch<ApiaryActionTypes>
+) => {
+  dispatch({
     type: ADD_APIARY,
     payload: newApiary,
-  };
+  });
 };
