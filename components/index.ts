c114611d27f9137f3c5d2742690faae5eacfecbe
Iga Zakrzewska <igazakrzewska@MacBook-Air-Iga.local>
2020-06-28 18:41:03 +0200

    add react-hooks-form

--- components/index.ts
+++ components/index.ts
@@ -2,3 +2,4 @@ export { Layout } from './Layout/Layout';
 export { ApiaryCard } from './ApiaryCard/ApiaryCard';
 export { Text } from './Text/Text';
 export { MenuIcon } from './MenuIcon/MenuIcon';
+export { Input } from './Input/Input';
--- screens/NewApiaryScreen.tsx
+++ screens/NewApiaryScreen.tsx
@@ -1,10 +1,70 @@
 import React from 'react';
-import { Layout, Text } from '../components';
+import { useDispatch } from 'react-redux';
+import { View, StyleSheet } from 'react-native';
+import { Input, Text } from '../components';
+import { formData } from '../utils';
+import { TouchableHighlight } from 'react-native-gesture-handler';
+import { addApiary } from '../store/actions/apiaries';
 
-export const NewApiaryScreen = () => {
+export const NewApiaryScreen = ({ navigation }) => {
+  const dispatch = useDispatch();
+  const [formValues, handleFormValueChange] = formData({
+    name: '',
+  });
+
+  const onApiaryAdded = () => {
+    const newApiary = {
+      id: 4,
+      name: formValues.name,
+      coords: { lat: 56, lng: 17 },
+      beehives: [
+        {
+          row: 1,
+          number: 1,
+          color: 'red',
+          active: true,
+          status: [
+            {
+              name: 'noQueen',
+            },
+          ],
+        },
+        {
+          row: 1,
+          number: 2,
+          color: 'blue',
+          active: true,
+          status: [
+            {
+              name: 'weakQueen',
+            },
+          ],
+        },
+      ],
+    };
+
+    dispatch(addApiary(newApiary));
+    navigation.navigate('ApiariesStack');
+  };
+
   return (
-    <Layout>
-      <Text>Nowa pasieka</Text>
-    </Layout>
+    <View style={styles.container}>
+      <Input
+        label='Nazwa pasieki'
+        formKey='name'
+        placeholder='Nazwa nowej pasieki'
+        handleChange={handleFormValueChange}
+      />
+      <TouchableHighlight onPress={onApiaryAdded}>
+        <Text>Dodaj pasiekę</Text>
+      </TouchableHighlight>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    margin: 20,
+  },
+});
